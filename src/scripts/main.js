39ebc30309b673316bcc14b64c5d6f4301ad76e8
Sam Pita <samanthakpita@gmail.com>
2019-10-25 10:52:42 -0500

    added exercise instructions

---
+++ src/scripts/main.js
@@ -0,0 +1,13 @@
+// Create an HTML page that contains a text area and a button labeled Create.
+
+
+// When the user enters in text into the text area and then clicks the create button, use a factory function that creates a new DOM component that has a border, and includes it's own delete button.
+
+
+// Insert that new component into the DOM.
+
+
+// When the user clicks the Delete button, the containing card, and no other cards, should then be removed from the DOM. Not just made invisible, actually removed from the DOM.
+
+
+// Pro tip: The card's id attribute, and the button's id attribute should share some common value. Then, when the button is clicked, find the corresponding parent DOM component. Remember the split() method on a string? That will be helpful.
